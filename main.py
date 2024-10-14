66e8a3a7b3d19bab7873540f4b8885be359bf82e
iitianick01 <iitianick2@gmail.com>
2024-10-14 21:43:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '6932739943:AAGS6C367Pxsbf0OFDvLDC6YJXHuK5biWn8'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [6678353955]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
